3f70e9e849b16444b5be19c6f32b77164ec09a00
Kely Wang <kkwang@andrew.cmu.edu>
2022-02-06 09:41:21 -0500

    implemented random music

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-// get dad joke element
+/* display a random dad joke */
+
+// get html element
 const jokeElem = document.getElementById("dadJoke");
 
 // define html request
@@ -18,5 +20,12 @@ fetch(url, options)
       jokeElem.innerText = result.joke;
   });
 
-// play dad music
-new Audio("minion-laugh.mp3").play();
+/* play a random dad song */
+
+// function to return a random integer from min to max (inclusive)
+function randInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+// play song
+new Audio(`songs/song${randInt(1, 10)}.mp3`).play();
